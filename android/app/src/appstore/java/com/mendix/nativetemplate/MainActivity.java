81fa82bd31c056850049aaba88f84a46ef330728
magnusgreennl <61319657+magnusgreennl@users.noreply.github.com>
2023-06-08 14:27:56 +0200

    [NBUI] Updating to Native Template v7.0.1

--- android/app/src/appstore/java/com/mendix/nativetemplate/MainActivity.java
+++ android/app/src/appstore/java/com/mendix/nativetemplate/MainActivity.java
@@ -1,12 +1,10 @@
 package com.mendix.nativetemplate;
 
-import android.os.Bundle;
-import android.os.PowerManager;
-import android.view.WindowManager;
 import android.os.Build;
-import android.view.View;
+import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import com.mendix.mendixnative.activity.MendixReactActivity;
 import com.mendix.mendixnative.config.AppUrl;
@@ -17,22 +15,17 @@ public class MainActivity extends MendixReactActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         this.getLifecycle().addObserver(new MendixActivityObserver(this));
-        Boolean hasDeveloperSupport = ((MainApplication) getApplication()).getUseDeveloperSupport();
+        boolean hasDeveloperSupport = ((MainApplication) getApplication()).getUseDeveloperSupport();
         mendixApp = new MendixApp(AppUrl.getUrlFromResource(this), MxConfiguration.WarningsFilter.none, hasDeveloperSupport, false);
-        PowerManager powerManager = (PowerManager) getSystemService(POWER_SERVICE);
-        PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,"MyApp::MyWakelockTag");
-        wakeLock.acquire();
         super.onCreate(savedInstanceState);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        View decorView =getWindow().getDecorView();
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.KITKAT) {
-            decorView.setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    |View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    |View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    |View.SYSTEM_UI_FLAG_HIDE_NAVIGATION//hide nav bar
-                    |View.SYSTEM_UI_FLAG_FULLSCREEN//hide status bar
-                    |View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        } 
+
+        // Checks the current theme and apply the correct style (Backwards compatible)
+        boolean isDarkMode;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            isDarkMode = getResources().getConfiguration().isNightModeActive();
+        } else {
+            isDarkMode = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES;
+        }
+        setTheme(isDarkMode ? R.style.AppTheme : R.style.AppTheme_Dark);
     }
 }
